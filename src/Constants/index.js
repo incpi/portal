0daa774dbeebd3dff7f7c41e235b6c0218292df9
Om <87596092+incpi@users.noreply.github.com>
2024-01-22 01:56:26 +0530

    Tailwind+Reactjs trial setups

---
+++ src/Constants/index.js
@@ -0,0 +1,45 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+];
+
+export const main = {
+  "Name": "Om's Portal",
+}
+
+// export const socialMedia = [
+//   {
+//     id: "social-media-1",
+//     icon: "instagram",
+//     link: "https://www.instagram.com/",
+//   },
+//   {
+//     id: "social-media-2",
+//     icon: "facebook",
+//     link: "https://www.facebook.com/",
+//   },
+//   {
+//     id: "social-media-3",
+//     icon: "twitter",
+//     link: "https://www.twitter.com/",
+//   },
+//   {
+//     id: "social-media-4",
+//     icon: "linkedin",
+//     link: "https://www.linkedin.com/",
+//   },
+// ];
